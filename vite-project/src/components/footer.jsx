6ede41e4b4f144cf1adc2eb8d5ea6297660092c7
balaji <balajimuralidharan200430@gmail.com>
2026-01-07 17:49:23 +0530

    Footer,Nav-Bar Responsive

--- vite-project/src/components/footer.jsx
+++ vite-project/src/components/footer.jsx
@@ -1,11 +1,81 @@
 import React from 'react';
 import "./footer.css";
+import logo from '../assets/Viworktech-logos/Viwork-tech-logo-purple-removebg-preview.png';
 
 
 export default function Footer() {
 
   return (
-      
-      <p></p>
+      <>
+       <div className='Footer-Container'>
+
+        <div className='Footer-Grid'>
+
+          <div className='Footer-Section1'>
+
+            <div className='Company-Content'>
+              <img src={logo} className='Footer-Viworktech-img'></img>
+              <h2 className='Footer-Header'>ViWork Tech</h2>
+            </div>
+            
+
+            <div className='Footer-Brief'>
+              <p className='Footer-Brief-Paragraph'>Making workflow automation accessible for everyone, everywhere.</p>
+            </div>
+
+            <div className='Social-Media-Handle'>
+              <div className='Social-Media-Box'>IN</div>
+              <div className='Social-Media-Box'>LI</div>
+              <div className='Social-Media-Box'>X</div>
+            </div>
+          </div>
+
+          <div className='Footer-Section2'>
+            
+              <h4 className='Header-Product'>Product</h4>
+            
+
+            <ul className='Footer-List-Container'>
+              <li className='Footer-List-Links'><a href="#">Features</a></li>
+              <li className='Footer-List-Links'><a href="#">Pricing</a></li>
+              <li className='Footer-List-Links'><a href="#">Integrations</a></li>
+              <li className='Footer-List-Links'><a href="#">Changelog</a></li>
+            </ul>
+
+          </div>
+
+          <div className='Footer-Section3'>
+           
+              <h4 className='Header-Product'>Company</h4>
+            
+
+            <ul className='Footer-List-Container'>
+              <li className='Footer-List-Links'><a href="#">About Us</a></li>
+              <li className='Footer-List-Links'><a href="#">Careers</a></li>
+              <li className='Footer-List-Links'><a href="#">Blog</a></li>
+              <li className='Footer-List-Links'><a href="#">Contact</a></li>
+            </ul>
+
+          </div>
+
+          <div className='Footer-Section4'>
+              <h4 className='Header-Product'>Legal</h4>
+            
+
+            <ul className='Footer-List-Container'>
+              <li className='Footer-List-Links'><a href="#">Privacy</a></li>
+              <li className='Footer-List-Links'><a href="#">Terms</a></li>
+              <li className='Footer-List-Links'><a href="#">Security</a></li>
+            </ul>
+
+          </div>
+
+
+
+        </div>
+         
+       </div>
+      </>
+     
   );
 }
--- vite-project/src/components/navbar.jsx
+++ vite-project/src/components/navbar.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import logo from '../assets/Viworktech-logos/Viwork-tech-logo-purple-removebg-preview.png';
 import { Link } from 'react-router-dom';
-
+import menuIcon from '../assets/Viworktech-logos/menu.png';
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-      <div className='Nav-bar'>
+    <div className='Nav-bar'>
 
         <div className='Navbar-Section-Left'>
           <img src={logo} className='Viworktech-logo' />
           <p className='Viworktech-Name'>ViWork Tech</p>
         </div>
 
-        <div className='Navbar-Section-Middle'>
-          <Link to="/Home" className='Link-container'>Home</Link>
-          <Link to="vite-project/src/pages/AboutUs.jsx" className='Link-container'>About Us</Link>
-          <Link to="/ContactUs" className='Link-container'>Contact</Link>
-          <Link to="/Feature" className='Link-container'>Feature</Link>
+      <div className='Navbar-Section-Middle'>
+          <div className='desktop-links'>
+            <Link to="/Home" className='Link-container'>Home</Link>
+            <Link to="vite-project/src/pages/AboutUs.jsx" className='Link-container'>About Us</Link>
+            <Link to="/ContactUs" className='Link-container'>Contact</Link>
+            <Link to="/Feature" className='Link-container'>Feature</Link>
+            </div>
+          
+        
+
+        <div className='mobile-menu'>
+          <img src={menuIcon} className='menu-icon' onClick={toggleMenu}  />
+          {isMenuOpen && (
+            <div className='dropdown-menu'>
+              <Link to="/Home" className='Link-container' onClick={toggleMenu}>Home</Link>
+              <Link to="vite-project/src/pages/AboutUs.jsx" className='Link-container' onClick={toggleMenu}>About Us</Link>
+              <Link to="/ContactUs" className='Link-container' onClick={toggleMenu}>Contact</Link>
+              <Link to="/Feature" className='Link-container' onClick={toggleMenu}>Feature</Link>
+              <Link to="/ContactUs" onClick={toggleMenu}>
+                <button className='Contact-Us-btn'>Contact Us</button>
+              </Link>
+            </div>
+          )}
         </div>
+      </div>
 
         <div className='Navbar-Section-Right'>
           <Link to="/ContactUs">
@@ -28,6 +52,6 @@ export default function Navbar() {
         </div>
 
 
-      </div>
+    </div>
   );
 }
